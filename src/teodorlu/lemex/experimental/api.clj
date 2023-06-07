0d17c6d3c4c2c426ddc8d53a2974b637555694d1
Teodor Heggelund <git@teod.eu>
2023-06-07 16:20:11 +0200

    Namespace structure

---
+++ src/teodorlu/lemex/experimental/api.clj
@@ -0,0 +1,6 @@
+;; # lemex API
+;;
+;; An application programming interface for creating your own little memex.
+
+(ns teodorlu.lemex.experimental.api)
+
---
+++ src/teodorlu/lemex/experimental/cli.clj
@@ -0,0 +1,6 @@
+;; # lemex CLI
+;;
+;; A command line interface for creating your own little memex.
+
+(ns teodorlu.lemex.experimental.cli)
+
