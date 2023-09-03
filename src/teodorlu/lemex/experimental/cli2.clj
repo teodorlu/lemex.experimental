1295aa6898697c3ca2690a655c0682b1be971684
Teodor Heggelund <git@teod.eu>
2023-09-03 15:33:28 +0200

    Code for adding providers---and shuffling

--- src/teodorlu/lemex/experimental/cli2.clj
+++ src/teodorlu/lemex/experimental/cli2.clj
@@ -52,73 +52,6 @@
   (when-let [provider-fn (:fn provider)]
     ((eval provider-fn))))
 
-(let [example-provider (quote {:fn (fn []
-                                     (let [data [["Simple Made Easy" "SxdOUGdseq4"]
-                                                 ["The Language of the System" "ROor6_NGIWU"]
-                                                 ["Maybe Not" "YR5WdGrpoug"]
-                                                 ["Design, Composition, and Performance" "QCwqnjxqfmY"]
-                                                 ["Design in Practice" "c5QF2HjHLSE"]
-                                                 ["The Value of Values" "-I-VpPMzG7c"]]]
-                                       (for [[title url] data]
-                                         {:title title :url url})))})]
-  (provider-links example-provider))
-
-(let [baseurl "https://play.teod.eu"
-      index-url (str baseurl "/index/big.edn")
-      page-url (fn [slug]
-                 (str baseurl "/" slug "/"))]
-  (->>
-   (edn/read-string (slurp index-url))
-   (filter (fn [page]
-             (and
-              (= :en (:lang page))
-              (= :ready-for-comments
-                 (:readiness page)))))
-   (map (fn [page] (select-keys page [:title :slug])))
-   (map (fn [{:keys [title slug]}]
-          {:title title
-           :url (page-url slug)}))))
-
-(let [example-provider (quote {:fn
-                               (fn []
-                                 (let [baseurl "https://play.teod.eu"
-                                       index-url (str baseurl "/index/big.edn")
-                                       page-url (fn [slug]
-                                                  (str baseurl "/" slug "/"))]
-                                   (->>
-                                    (edn/read-string (slurp index-url))
-                                    (filter (fn [page]
-                                              (and
-                                               (= :en (:lang page))
-                                               (= :ready-for-comments
-                                                  (:readiness page)))))
-                                    (map (fn [page] (select-keys page [:title :slug])))
-                                    (map (fn [{:keys [title slug]}]
-                                           {:title title
-                                            :url (page-url slug)})))))})]
-  (provider-links example-provider))
-
-;; => (nil :deprecated :forever-incomplete :noindex :ready-for-comments :wtf-is-this)
-
-(let [links-url "https://play.teod.eu/index/big.edn"
-      example-provider (quote {:fn (fn []
-
-                                     (let [data [["Simple Made Easy" "SxdOUGdseq4"]
-                                                 ["The Language of the System" "ROor6_NGIWU"]
-                                                 ["Maybe Not" "YR5WdGrpoug"]
-                                                 ["Design, Composition, and Performance" "QCwqnjxqfmY"]
-                                                 ["Design in Practice" "c5QF2HjHLSE"]
-                                                 ["The Value of Values" "-I-VpPMzG7c"]]]
-                                       (for [[title url] data]
-                                         {:title title :url url})))})]
-  (provider-links example-provider))
-
-(defn url->inferred-slug [url]
-  (-> url
-      (str/split #"/")
-      last
-      (str/replace #"\.edn$" "")))
-
 (defn providers-path []
   (fs/file (fs/xdg-config-home "teodorlu.lemex.experimental")
            "providers.d"))
@@ -127,28 +60,35 @@
   (fs/file (providers-path)
            (str slug ".edn")))
 
-
-(->>
- (fs/list-dir (providers-path))
- (map #(-> {:slug (str/replace (fs/file-name %) #"\.edn$" "")})))
-
 (defn providers []
   (->>
    (fs/list-dir (providers-path))
    (map #(-> {:slug (str/replace (fs/file-name %) #"\.edn$" "")}))))
 
 
-(defn add-provider [url]
+(defn add-provider [slug url]
   (fs/create-dirs (providers-path))
-  (let [slug (url->inferred-slug url)]
-    (spit (provider-path slug) (slurp url))))
+  (spit (provider-path slug) (slurp url)))
+
+(defn provider-links2 [provider]
+  (let [path (provider-path (:slug provider))]
+    (when (fs/exists? path)
+      (when-let [provider-fn (:fn (edn/read-string (slurp path)))] ((eval provider-fn))))))
 
 (defn -main [& args]
   (println "lemex"))
 
 (comment
-  (add-provider "https://raw.githubusercontent.com/teodorlu/lemex.experimental/master/contrib/provider.d/rich-hickey-greates-hits.edn")
-  (providers))
+  (add-provider "rich-hickey-greatest-hits"
+                "https://raw.githubusercontent.com/teodorlu/lemex.experimental/master/contrib/provider.d/rich-hickey-greatest-hits.edn")
+  (add-provider "play.teod.eu"
+                "https://raw.githubusercontent.com/teodorlu/lemex.experimental/master/contrib/provider.d/play.teod.eu.edn")
+
+  (->> (providers)
+       shuffle
+       first
+       provider-links2)
+  )
 
 ;; lemex roulette --browse -- get dropped into a random place
 
